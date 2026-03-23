17dff1426c5eb60ece257c3bbbd09573f642ad8d
Seongwon_P <ikaros364125@kyonggi.ac.kr>
2026-03-23 17:48:55 +0900

    refactor: app entry structure
    엔트리 구조 정리
    불필요한 App.css 제거
    index.html > index.tsx > main.tsx 구조에서 index.tsx는 main.tsx로 흡수됨. 통일성을 위해 index.css는 main.css로 이름만 변경됨.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./main.css";
 import AppProviders from "./app/providers/AppProviders";
 import AppRouter from "./app/router/AppRouter";
 
-export default function Main() {
+function Main() {
   return (
-    <AppProviders>
-      <AppRouter />
-    </AppProviders>
+    <React.StrictMode>
+      <AppProviders>
+        <AppRouter />
+      </AppProviders>
+    </React.StrictMode>
   );
 }
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<Main />);
